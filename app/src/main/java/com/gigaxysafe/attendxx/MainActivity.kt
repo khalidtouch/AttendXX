f79154ff78099a47e35d9a99c5c76c095bfdb616
Khalid Isah <khalidhulabaloo9@gmail.com>
2022-07-16 12:07:20 +0100

    starting out

---
+++ app/src/main/java/com/gigaxysafe/attendxx/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.gigaxysafe.attendxx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gigaxysafe.attendxx.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+}
